2a378bb9122377723cde448387d895959e81b5e7
Thomas Kolb <cfr34k-github@tkolb.de>
2015-01-24 17:23:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+= Freifunk-Bot =
+
+Ein IRC-Bot für Freifunk-Netze, geschrieben in Python 3.
+
+== Aktueller Stand ==
+
+Die nodes.json einer Freifunk-Community wird regelmäßig abgefragt und Änderungen werden im Channel angezeigt. Dazu gehören:
+
+- Neue Knoten
+- Gelöschte Knoten
+- Änderungen im online/offline-Status
+
+== Abhängigkeiten ==
+
+Python-Module:
+
+- irc ( https://pypi.python.org/pypi/irc )
+- requests ( https://pypi.python.org/pypi/requests/2.5.1 )
---
+++ freifunk_bot.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+# vim: noexpandtab ts=2 sw=2 sts=2
+
+import irc.client
+import requests
+import time
+import sys
+import threading
+
+INTERVAL = 60
+
+class Node:
+	nid = ''
+	name = ''
+	online = False
+
+class FreifunkBot(irc.client.SimpleIRCClient):
+	def __init__(self, target):
+		irc.client.SimpleIRCClient.__init__(self)
+		self.target = target
+
+		# map of id => node data
+		self.known_nodes = {}
+
+		self.timer = threading.Thread(target=self.scheduler, daemon=True);
+
+	def on_welcome(self, connection, event):
+		if irc.client.is_channel(self.target):
+			connection.join(self.target)
+		elif not self.timer.is_alive():
+			self.timer.start()
+
+	def on_join(self, connection, event):
+		if not self.timer.is_alive():
+			self.timer.start()
+
+	def on_disconnect(self, connection, event):
+		self.timer.stop()
+		sys.exit(0)
+
+	def on_privmsg(self, connection, event):
+		print("PRIVMSG {}".format(event.arguments[0]))
+
+	def scheduler(self):
+		while True:
+			self.do_freifunk_cycle()
+			time.sleep(INTERVAL)
+
+	def do_freifunk_cycle(self):
+		r = requests.get('http://ingolstadt.freifunk.net/map/nodes.json')
+		#r = requests.get('http://localhost:8888/nodes.json')
+		json = r.json()
+
+		current_nodes = {}
+		for node in json['nodes']:
+			n = Node()
+			n.nid = node['id']
+			n.name = node['name']
+			n.online = node['flags']['online']
+
+			current_nodes[n.nid] = n
+
+		if not self.known_nodes:
+			# first load
+			self.known_nodes = current_nodes
+			msg = "ist initialisiert: {:d} bekannte Knoten".format(len(current_nodes))
+			self.connection.action(self.target, msg)
+
+		current_nids = set(current_nodes.keys())
+		known_nids = set(self.known_nodes.keys())
+
+		new_nodes  = list(current_nids - known_nids)
+		gone_nodes = list(known_nids - current_nids)
+
+		changed_nodes = []
+		for nid, node in current_nodes.items():
+			if nid in self.known_nodes.keys():
+				if self.known_nodes[nid].online != current_nodes[nid].online:
+					changed_nodes.append(nid)
+
+		for nid in new_nodes:
+			msg = "Neuer Knoten: {:s}".format(current_nodes[nid].name)
+			self.connection.privmsg(self.target, msg)
+
+		for nid in gone_nodes:
+			msg = "Knoten gelöscht: {:s}".format(self.known_nodes[nid].name)
+			self.connection.privmsg(self.target, msg)
+
+		for nid in changed_nodes:
+			msg = "{:s} ist jetzt {}".format(
+					current_nodes[nid].name,
+					"online" if current_nodes[nid].online else "offline")
+			self.connection.privmsg(self.target, msg)
+
+		self.known_nodes = current_nodes
+
+def main():
+	if len(sys.argv) != 4:
+		print("Usage: freifunk_bot.py <server[:port]> <nickname> <target>")
+		print("\ntarget is a nickname or a channel.")
+		sys.exit(1)
+
+	s = sys.argv[1].split(":", 1)
+	server = s[0]
+	if len(s) == 2:
+		try:
+			port = int(s[1])
+		except ValueError:
+			print("Error: Erroneous port.")
+			sys.exit(1)
+	else:
+		port = 6667
+	nickname = sys.argv[2]
+	target = sys.argv[3]
+
+	c = FreifunkBot(target)
+	try:
+		c.connect(server, port, nickname)
+	except irc.client.ServerConnectionError as x:
+		print(x)
+		sys.exit(1)
+	c.start()
+
+if __name__ == "__main__":
+	main()
