17847bb09a952498ebfdb5852b572fd1b26a9f94
Thomas Kolb <thomas@tkolb.de>
2015-02-01 17:25:53 +0100

    Set plot output location from config.py

--- plot/network.py
+++ plot/network.py
@@ -3,31 +3,66 @@
 
 import pylab as p
 import time
+import zlib
 
 import sys, os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import config
 
-def plot(timestamp, ydata, ylabel, title, output_file):
-	f = p.figure()
+COLORS=[]
 
-	p.step(timestamp, ydata, 'r', linewidth=2)
+for r in [0x00, 0x55, 0xaa]:
+	for g in [0x00, 0x55, 0xaa]:
+		for b in [0x00, 0x55, 0xaa]:
+			c = (r << 16) + (g << 8) + b
+			cs = '#{:06x}'.format(c)
+			COLORS.append(cs)
+
+def plot(timestamp, ydata, ylabel, title, color, output_file):
+	f = p.figure(figsize=(4.5, 3))
+
+	p.step(timestamp, ydata, 'r', linewidth=2, color=color)
 
 	p.axis('tight')
 
 	p.ylabel(ylabel)
 	p.xlabel('Zeit')
 	p.title(title)
+	p.grid()
 
 	# adjust xticks
-	ticks = p.xticks()[0]
-	ticks = ticks[0:len(ticks):2]
-	textticks = [time.strftime('%Y-%m-%d\n%H:%M', time.gmtime(t)) for t in ticks]
+	if len(timestamp) >= 2:
+		r = max(timestamp) - min(timestamp)
+		if r > 30*86400:
+			tickdist = 30*86400
+		elif r > 7*86400:
+			tickdist = 7*86400
+		elif r > 86400:
+			tickdist = 86400
+		elif r > 12*3600:
+			tickdist = 6*3600
+		elif r > 4*3600:
+			tickdist = 3*3600
+		else:
+			tickdist = 3600
+
+		start = p.floor(min(timestamp)/tickdist)*tickdist
+		ticks = p.arange(start, max(timestamp), tickdist)
+	else:
+		ticks = p.xticks()[0]
+		ticks = ticks[0:len(ticks):3]
+	textticks = [time.strftime('%m-%d\n%H:%M', time.gmtime(t)) for t in ticks]
 
 	p.xticks(ticks, textticks)
 	f.savefig(output_file)
 
+# load node names
+nodenames = {}
+with open(config.LOG_NODENAMES, 'r') as nodefile:
+	for line in nodefile:
+		nid, name = line.split(' ', 1)
+		nodenames[nid] = name.strip()
 
 # global nodes
 timestamp = []
@@ -43,7 +78,8 @@ with open(config.LOG_NODECOUNT, 'r') as logfile:
 	     nodes,
 	     'Knoten',
 	     'Knoten im Netzwerk',
-	     '/tmp/nodes.svg')
+	     COLORS[0],
+	     os.path.join(config.PLOT_DIR, 'nodes.svg'))
 
 # global online nodes
 timestamp = []
@@ -59,7 +95,8 @@ with open(config.LOG_ONLINENODECOUNT, 'r') as logfile:
 	     nodes,
 	     'Knoten',
 	     'Knoten online',
-	     '/tmp/nodes_online.svg')
+	     COLORS[0],
+	     os.path.join(config.PLOT_DIR, 'nodes_online.svg'))
 
 # global clients
 timestamp = []
@@ -75,7 +112,8 @@ with open(config.LOG_TOTALCLIENTCOUNT, 'r') as logfile:
 	     clients,
 	     'Clients',
 	     'Clients im Netz',
-	     '/tmp/clients.svg')
+	     COLORS[0],
+	     os.path.join(config.PLOT_DIR, 'clients.svg'))
 
 # clients for each node
 clientdata = {}
@@ -85,18 +123,26 @@ with open(config.LOG_NODECLIENTCOUNT, 'r') as logfile:
 		data = line.strip().split(' ')
 
 		timestamp = int(data[0])
-		name = data[1]
+		nid = data[1]
 		clients = int(data[2])
 
-		if name not in clientdata.keys():
-			clientdata[name] = {'timestamp': [], 'clients': []}
+		if nid not in clientdata.keys():
+			clientdata[nid] = {'timestamp': [], 'clients': []}
 
-		clientdata[name]['timestamp'].append(timestamp)
-		clientdata[name]['clients'].append(clients)
+		clientdata[nid]['timestamp'].append(timestamp)
+		clientdata[nid]['clients'].append(clients)
 
-	for name, data in clientdata.items():
+	for nid, data in clientdata.items():
+		if nid in nodenames.keys():
+			name = nodenames[nid]
+		else:
+			name = '[' + nid + ']'
+
+		color = COLORS[zlib.crc32(bytes(nid, 'ascii')) % len(COLORS)]
+
 		plot(data['timestamp'],
-				 data['clients'],
-				 'Clients',
-				 'Clients an Knoten {}'.format(name),
-				 '/tmp/clients_{}.svg'.format(name))
+		     data['clients'],
+		     'Clients',
+		     'Clients an Knoten {}'.format(name),
+		     color,
+		     os.path.join(config.PLOT_DIR, 'clients_{}.svg'.format(nid)))
